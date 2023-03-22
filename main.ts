ab1a9acf3ea9614f9cad4a76c33db4b9d131653f
迷渡 <justjavac@gmail.com>
2023-03-22 15:45:44 +0800

    donot modify Headers

--- main.ts
+++ main.ts
@@ -5,9 +5,5 @@ const OPENAI_API_HOST = "api.openai.com";
 serve(async (request) => {
   const url = new URL(request.url);
   url.host = OPENAI_API_HOST;
-
-  request.headers.delete("x-forwarded-for");
-  request.headers.delete("x-real-ip");
-
   return await fetch(url, request);
 });
