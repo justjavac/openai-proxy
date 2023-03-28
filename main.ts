f5bcd4fdfe0c3069be48b1479e72cb17968f7fd3
迷渡 <justjavac@gmail.com>
2023-03-28 08:48:06 +0800

    upgrade 0.181.0

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
 const OPENAI_API_HOST = "api.openai.com";
 
